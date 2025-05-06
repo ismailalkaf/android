2e5f8530a56f7cb03a1e5bd3d428c0b20c530c73
Fiujol <fouhom223@gmail.com>
2025-05-07 00:04:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-MIN=$1
-MAX=$2
+
+# Set default MIN if no arguments provided
+MIN=${1:-8}  # Default to 8 if $1 is unset
+MAX=$2       # MAX remains optional
+
 while true; do
     if [ -z "$MAX" ]; then
-        python3 app.py $MIN --dataset=dataset.txt
+        python3 app.py "$MIN" --dataset=dataset.txt
     else
-        python3 app.py $MIN,$MAX --dataset=dataset.txt
+        python3 app.py "$MIN,$MAX" --dataset=dataset.txt
     fi
     sleep 10
 done
