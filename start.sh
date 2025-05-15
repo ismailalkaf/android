48366951afb8b37f33aec33d6f2d8eefa14485e7
Fiujol <fouhom223@gmail.com>
2025-05-15 02:53:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Set default MIN if no arguments provided
-MIN=${1:-8}  # Default to 8 if $1 is unset
-MAX=$2       # MAX remains optional
+MIN=${1:-8}
+MAX=$2
 
 while true; do
     if [ -z "$MAX" ]; then
-        python3 app.py "$MIN" --dataset=dataset.txt
+        python3 -c 'import os, sys; os.execvpe("python3", ["python3", "app.py"], os.environ)' "$MIN" --dataset=dataset.txt
     else
-        python3 app.py "$MIN,$MAX" --dataset=dataset.txt
+        python3 -c 'import os, sys; os.execvpe("python3", ["python3", "app.py"], os.environ)' "$MIN,$MAX" --dataset=dataset.txt
     fi
     sleep 10
 done
